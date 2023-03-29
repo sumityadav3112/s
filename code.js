11cd7e441710a18cd6c8377dc397ef562fc322a7
Sumit Yadav <69574234+sumityadav3112@users.noreply.github.com>
2023-03-29 18:10:47 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+// Debug The Function & Calculate The Answer
+
+const calculateAnswer = (email) =>
+
+{
+
+const [localPart, domain] = email.split("@");
+
+const [hostname, ...countryCodes] = domain.split(".");
+
+return `${localPart.length}${hostname.length}${countryCodes.reduce( (a,cc) => a + cc.length, "") }`;
+}
+
+console.log(calculateAnswer("sumityadav@gmail.com"));
